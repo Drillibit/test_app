56e4cdac3de06e6ed4b893ba37badd1699ece965
Drillibit <293344asd@gmail.com>
2018-06-23 19:03:04 +0600

    select field

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -15,7 +15,47 @@ export default class Main extends Component {
     };
   }
 
+  onTextFieldChange = e => {
+    const { value, name } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
   render() {
-    return <div className="app_wrapper">Task App Main Component</div>;
+    const { taskName, taskDescription, taskImportance, taskDone } = this.state;
+    return (
+      <div className="">
+        <p>Task App Main Component</p>
+        <form>
+          <label htmlFor="taskName">Задача</label>
+          <input
+            type="text"
+            name="taskName"
+            value={taskName}
+            onChange={this.onTextFieldChange}
+          />
+          <label htmlFor="taskDescription">Описание</label>
+          <input
+            type="text"
+            name="taskDescription"
+            value={taskDescription}
+            onChange={this.onTextFieldChange}
+          />
+          <label htmlFor="taskImportance">Важность задачи</label>
+          <select
+            name="taskImportance"
+            value={taskImportance}
+            onChange={this.onTextFieldChange}
+          >
+            <option value="regular">Обычная</option>
+            <option value="important">Важная</option>
+            <option value="highly_important">Очень важная</option>
+          </select>
+          <label htmlFor="taskDone">Сделано</label>
+          <input name="taskDone" type="checkbox" checked={taskDone} />
+        </form>
+      </div>
+    );
   }
 }
